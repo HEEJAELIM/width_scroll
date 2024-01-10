cdac398ad334103d714e2cec4686ccbe9dfdcdbe
heejae Lim <heejae0529@naver.com>
2024-01-10 18:25:29 +0900

    end

---
+++ main.js
@@ -0,0 +1,21 @@
+const target = document.querySelectorAll(".timeline li div");
+
+window.addEventListener("load", init);
+
+function init() {
+  setEqualHeight(target);
+}
+
+function setEqualHeight(el) {
+  let initHeight = 0;
+  for (let i = 0; i < el.length; i++) {
+    // 높이 구하기
+    let tallestHgt = el[i].offsetHeight;
+    if (initHeight < tallestHgt) {
+      initHeight = tallestHgt;
+    }
+  }
+  for (let item of target) {
+    item.style.height = `${initHeight}px`;
+  }
+}
